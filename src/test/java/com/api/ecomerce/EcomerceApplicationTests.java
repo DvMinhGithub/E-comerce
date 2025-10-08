031216ed26b6c35324cbb188a5b3c2b31ca59b8c
Minh Dinh <minhdv.dev@gmail.com>
2025-10-08 17:37:02 +0700

    feat: Initial project setup and structure

---
+++ src/test/java/com/api/ecomerce/EcomerceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.api.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
